cf3aed9207ed5ce66e5e2bf890d015632ff2e3dc
ekamleen <ekamleenm@gmail.com>
2024-06-14 00:26:50 -0700

    mcts

--- Tic-Tac-Toe/mcts.py
+++ Tic-Tac-Toe/mcts.py
@@ -32,3 +32,44 @@ class TreeNode:
         # init current node's children
         self.children = {}
 
+
+# MCTS class
+class MCTS:
+    # search for the best move in the current position
+    def search(self, initial_state):
+        # create root node
+        self.root = TreeNode(initial_state, None)
+
+        # search 1000 iterations
+        for iterations in range(1000):
+            # select a node --> SELECTION PHASE
+            node = self.select(self.root)
+
+            # SIMULATION PHASE
+            score = self.rollout(node.board)
+
+            # Backpropagation : the score the # visits and score to the root node
+            self.backpropogate(node, score)
+
+            # pickup the best move in the current position
+            try:
+                return self.get_best_move(self.root, 0)
+            except Exception as e:
+                pass
+
+    # select most promising node
+    def select(self, node):
+        pass
+
+    # simulate the game by making random moves until reach end of the game
+    def rollout(self, board):
+        pass
+
+    def backpropogate(self,node,score):
+        pass
+
+    # Select the best node bases on UCB1/UCT formula
+    def get_best_move(self,node,exploration_constant):
+        pass
+
+
